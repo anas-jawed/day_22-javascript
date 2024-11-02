3eb918feaf8b7dba378d987b56dae2c80babd5b9
Anas Jawed <anasjawedbmj@gmail.com>
2024-11-02 22:06:26 +0200

    Day 22 javascript

---
+++ app.js
@@ -0,0 +1,32 @@
+//  PRactise Questions
+
+// 1 . <p> with red text that says "hey i'm red"
+
+// let para = document.createElement('p');
+// para.innerHTML = "Hey I'm red";
+// let body = document.querySelector('body');
+// body.append(para);
+// para.classList.add('red');
+
+
+// 2. an h3 with blue text that says "I m a blue h3!"
+
+// let h3 = document.createElement('h3');
+// h3.innerHTML = "I'm a blue h3!";
+// let body = document.querySelector('body').append(h3);
+// h3.classList.add('blue');
+
+
+// 3. a div with a black border and pink background color with the following elements inside of it another h1 that says im a div a p that syas me too:
+
+// let div = document.createElement('div');
+// let h1 = document.createElement('h1');
+// let para = document.createElement('p');
+// h1.innerHTML = "I'm in a div";
+// para.innerHTML = "ME TOO!"
+
+// div.append(h1);
+// div.append(para);
+
+// let body = document.querySelector('body').prepend(div);
+// div.classList.add("div")
